ffd70a55d8f651c7da4e73fb97ac40b0b7379eff
pheck <pheck@live.com>
2020-08-07 14:24:45 -0700

    Added fish list

--- BlzTest/Data/HubbardCountyService.cs
+++ BlzTest/Data/HubbardCountyService.cs
@@ -1,29 +1,52 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace BlzTest.Data
 {
     public class HubbardCountyService
     {
-        private static readonly string[] FishList = new[]
+        private static readonly string[] FishData = new[]
         {
             "Sunfish", "LargeMouth", "SmallMouth", "Northern", "Walleye", "Perch", "Musky", "Bullhead", "BlueGill", "Crappie"
         };
 
-        private static readonly string[] LakeList = new[]
+        private static readonly string[] LakeData = new[]
         {
-            "Pickerel", "Blue", "Bottle", "Spider", "Bad Axe", "Mantrap", "Big Sand", "Ida", "Little Sand", "Potato"
+            "Pickerel:1,2,3,4,5,6", "Blue:1,4,5,6", "Bottle:1,9,10", "Spider:2,3,4", "Bad Axe:4,5,6,7", "Mantrap:4,5,6,7", "Big Sand:3,5", "Ida:1,6", "Little Sand:3,4,5,10", "Potato:4,7"
         };
 
         public Task<HubbardCounty[]> GetLakesAsync()
         {
             var rng = new Random();
-            return Task.FromResult(Enumerable.Range(1, 5).Select(index => new HubbardCounty
+            var rngIndex = rng.Next(1,10);
+
+            return Task.FromResult(Enumerable.Range(0, 9).Select(index => new HubbardCounty
             {
-                LakeName  = LakeList[ rng.Next(1,10) ],
-                FishList = FishList[ rng.Next(1,10) ]
+                LakeName  = LakeData[ index  ],
+                FishList = GetLakeFishList( index).ToString()
             }).ToArray());
+        }
+
+        private string GetLakeFishList(int lakeIndex){
+
+            var fishList = new StringBuilder();
+
+            var lakeDataRaw = LakeData[lakeIndex];
+            
+            var lakeDataTokens = lakeDataRaw.Split(':');
+            
+            var fishArr =  lakeDataTokens[1];
+            
+            var fishIndexTokens = fishArr.Split(',');
+
+            foreach(string index in fishIndexTokens){
+                int idx = int.Parse(index);
+                fishList.Append($"{FishData[ idx  -1 ].ToString()}");
+            }
+            return fishList.ToString();
         }
     }
 }
